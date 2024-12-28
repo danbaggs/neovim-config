52b5558b531166c856635d85f3335f6880bf224f
Dan Baggs <baggage@hotmail.co.uk>
2024-12-28 10:13:16 +0000

    update completion keymap

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -11,7 +11,9 @@ return {
       -- 'default' for mappings similar to built-in completion
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'super-tab',
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
