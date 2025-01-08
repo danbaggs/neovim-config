66753986a0df43648899297d6d37f719297c1e59
Dan Baggs <baggage@hotmail.co.uk>
2025-01-08 11:50:54 +0000

    update terminal size

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -58,11 +58,11 @@ vim.keymap.set("n", '<leader>dt', function()
   require('dap').terminate(); require('dapui').close();
 end)
 
--- open a terminal in a window of 40% of the current view width
+-- open a terminal in a window of 30% of the current view width
 -- useful for ultrawide dev
 vim.keymap.set("n", "<space>st", function()
   vim.cmd.vnew()
   vim.cmd.term()
-  vim.api.nvim_win_set_width(0, math.floor(vim.o.columns * 0.4))
+  vim.api.nvim_win_set_width(0, math.floor(vim.o.columns * 0.3))
 end
 )
