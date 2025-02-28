f623ceb6111060b6e63952fa449f43a152db851b
Dan Baggs <baggage@hotmail.co.uk>
2025-02-28 10:18:34 +0000

    revert blink completion command mode to old usage

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -14,6 +14,16 @@ return {
       keymap = {
         preset = 'super-tab',
       },
+      cmdline = {
+        keymap = {
+          preset = 'super-tab',
+        },
+        completion = {
+          menu = {
+            auto_show = true,
+          },
+        },
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
