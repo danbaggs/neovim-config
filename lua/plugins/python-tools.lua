a9f0a22b008afb947dabbd4ba9f448583f77c30a
Dan Baggs <dan@baggs.tech>
2025-10-03 19:13:31 +0100

    remove debuggers

--- lua/plugins/python-tools.lua
+++ lua/plugins/python-tools.lua
@@ -4,17 +4,17 @@ return {
     "stsewd/isort.nvim",
     ft = "python",
   },
-  {
-    -- https://github.com/mfussenegger/nvim-dap-python
-    'mfussenegger/nvim-dap-python',
-    ft = 'python',
-    dependencies = {
-      -- https://github.com/mfussenegger/nvim-dap
-      'mfussenegger/nvim-dap',
-    },
-    config = function()
-      -- Update the path passed to setup to point to your system or virtual env python binary
-      require('dap-python').setup('/usr/bin/python3')
-    end
-  }
+  -- {
+  --   -- https://github.com/mfussenegger/nvim-dap-python
+  --   'mfussenegger/nvim-dap-python',
+  --   ft = 'python',
+  --   dependencies = {
+  --     -- https://github.com/mfussenegger/nvim-dap
+  --     'mfussenegger/nvim-dap',
+  --   },
+  --   config = function()
+  --     -- Update the path passed to setup to point to your system or virtual env python binary
+  --     require('dap-python').setup('/usr/bin/python3')
+  --   end
+  -- }
 }
