9eae1c25cf0775068736a75ca30c44b19a37c3d6
Dan Baggs <baggage@hotmail.co.uk>
2026-05-16 11:45:13 +0100

    update to 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile", },
   config = function()
-    require 'nvim-treesitter.configs'.setup {
+    require 'nvim-treesitter'.setup {
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
       ensure_installed = {
         "bash",
